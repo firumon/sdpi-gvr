cb312f3364fb8f6693e550f10f562c1e2e6719bf
Firose Hussain <github@firumon.com>
2023-08-08 08:02:14 +0530

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# sdpi-gvr-member-gathering (sdpi-gvr-member-gathering)
+
+Gathering process of members of SDPI Guruvayur
+
+## Install the dependencies
+```bash
+yarn
+# or
+npm install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+
+### Build the app for production
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.config.js](https://v2.quasar.dev/quasar-cli-vite/quasar-config-js).
---
+++ src/boot/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA4977rYf4AUHvsm3dC-vQYZd0RPbI_gX8",
+  authDomain: "sdpi-2ff7a.firebaseapp.com",
+  projectId: "sdpi-2ff7a",
+  storageBucket: "sdpi-2ff7a.appspot.com",
+  messagingSenderId: "494959783669",
+  appId: "1:494959783669:web:aae93ec4480f58776c5fbc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
